6126e927b8ca75db160980509a30da04fe039ef7
Vishal Dhameliya <vishaldhameliyabb19798@gmail.com>
2024-02-01 19:44:56 +0530

    chapter 4

---
+++ ownership/target/debug/deps/ownership-a0c8775665123777.d
@@ -0,0 +1,5 @@
+D:\Vishal\Github\rust-learn\ownership\target\debug\deps\libownership-a0c8775665123777.rmeta: src\main.rs
+
+D:\Vishal\Github\rust-learn\ownership\target\debug\deps\ownership-a0c8775665123777.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/deps/ownership-d6f6ed48388ee3bd.d
@@ -0,0 +1,5 @@
+D:\Vishal\Github\rust-learn\ownership\target\debug\deps\libownership-d6f6ed48388ee3bd.rmeta: src\main.rs
+
+D:\Vishal\Github\rust-learn\ownership\target\debug\deps\ownership-d6f6ed48388ee3bd.d: src\main.rs
+
+src\main.rs:
